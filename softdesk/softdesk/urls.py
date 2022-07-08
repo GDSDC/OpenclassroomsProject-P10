34964826cb7cc4d5097bb531c68e3fbd3d0bdd8c
GDSDC <gdc.business.2019@gmail.com>
2022-07-08 14:52:31 +0200

    feat(users) : creating model User, views and url

--- softdesk/softdesk/urls.py
+++ softdesk/softdesk/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from users.views import SignUp, Login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('signup/', SignUp.as_view()),
+    path('login/', Login.as_view()),
 ]
