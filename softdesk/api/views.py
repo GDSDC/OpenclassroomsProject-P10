bf9e1e83b5d5ee9870b989b97785e3c7281688f3
GDSDC <gdc.business.2019@gmail.com>
2022-07-12 17:58:43 +0200

    style

--- softdesk/api/views.py
+++ softdesk/api/views.py
@@ -22,60 +22,6 @@ class SignUpAPIView(APIView):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    # def post(self, request):
-    #     data = dict(request.POST.items())
-    #     try:
-    #         email = data['email']
-    #         password1 = data['password1']
-    #         password2 = data['password2']
-    #         first_name = data['first_name']
-    #         last_name = data['last_name']
-    #
-    #         try:
-    #             validate_email(email)
-    #             if User.objects.filter(email=email).exists():
-    #                 return Response('EMAIL ALREADY EXISTS', status=status.HTTP_409_CONFLICT)
-    #
-    #             else:
-    #                 if password1 != password2:
-    #                     return Response('DIFFERENT PASSWORDS !', status=status.HTTP_400_BAD_REQUEST)
-    #                 else:
-    #                     new_user = User()
-    #                     new_user.email = email
-    #                     new_user.first_name = first_name
-    #                     new_user.last_name = last_name
-    #                     new_user.password = password1
-    #                     new_user.save()
-    #                     return Response(f'New user {email} created in database !', status=status.HTTP_201_CREATED)
-    #         except ValidationError:
-    #             return Response('INVALID EMAIL', status=status.HTTP_400_BAD_REQUEST)
-    #
-    #
-    #     except KeyError:
-    #         return Response('WRONG DATA FORMAT. Please enter email, first_name, last_name, and password.',
-    #                         status=status.HTTP_400_BAD_REQUEST)
-
-
-# class Login(APIView):
-#     """API class view for login in"""
-#
-#     def post(self, request):
-#         data = dict(request.POST.items())
-#         try:
-#             user_email = data['email']
-#             user_password = data['password']
-#
-#             user = authenticate(email=user_email, password=user_password)
-#             if user is not None and user.is_active:
-#                 login(request, user)
-#                 return Response(f'User {user_email} logged in successfully')
-#             else:
-#                 return Response('INVAVID LOGIN CREDENTIALS', status=status.HTTP_401_UNAUTHORIZED)
-#
-#
-#
-#         except KeyError:
-#             return Response('WRONG DATA FORMAT. Please enter email and password.', status=status.HTTP_400_BAD_REQUEST)
 
 class TestAuth(APIView):
     permission_classes = (IsAuthenticated,)
