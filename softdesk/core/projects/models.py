53decdcfb3c8a44ed820ab0aea1ce9d8bd7c997e
gabrieldacosta <gdc.business.2019@gmail.com>
2022-07-14 11:41:42 +0200

    feat(projects) : creating project model

--- softdesk/core/projects/models.py
+++ softdesk/core/projects/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+PROJECT_PERMISSIONS = [('EXEMPLE_A_COMPLETER','IDEM')]
+#TODO : remplir les permissions possibles pour les contributeurs dans le cadre d'un projet au format valide
+
+class Project(models.Model):
+    """Projects class"""
+
+    title = models.CharField(max_length=60, blank=True)
+    description = models.CharField(max_length=512, blank=True)
+    type = models.CharField(max_length=60, blank=True)
+    author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.SET_NULL)
