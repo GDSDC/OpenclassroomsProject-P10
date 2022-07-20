05dd225621e111294a16e43c8c553bdba6609603
GDSDC <gdc.business.2019@gmail.com>
2022-07-20 18:19:35 +0200

    fix(UserSerializer) : removing user_id from fields

--- softdesk/api/serializers.py
+++ softdesk/api/serializers.py
@@ -10,7 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['user_id', 'email', 'first_name', 'last_name', 'password']
+        fields = ['email', 'first_name', 'last_name', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
 
