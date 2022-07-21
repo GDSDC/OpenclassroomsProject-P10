eaacc640d8ce276e7d880e45e924420e1285810d
GDSDC <gdc.business.2019@gmail.com>
2022-07-21 19:43:57 +0200

    fix(issues) : updating priority in model and migrate

--- softdesk/core/issues/models.py
+++ softdesk/core/issues/models.py
@@ -13,10 +13,9 @@ class Issue(models.Model):
         IMPROVEMENT = 'I', _('Improvement')
 
     class Priority(models.TextChoices):
-        CRITICAL = 'C', _('Critical')
-        MAJOR = 'M', _('Major')
-        NORMAL = 'N', _('Normal')
-        MINOR = 'MN', _('Minor')
+        LOW = 'L', _('Low')
+        MEDIUM = 'M', _('Medium')
+        HIGH = 'H', _('High')
 
     class Status(models.TextChoices):
         TODO = 'TD', _('To Do')
@@ -26,7 +25,7 @@ class Issue(models.Model):
     title = models.CharField(max_length=60)
     desc = models.CharField(max_length=512, blank=True)
     tag = models.CharField(max_length=1, choices=Tag.choices)
-    priority = models.CharField(max_length=2, choices=Priority.choices)
+    priority = models.CharField(max_length=1, choices=Priority.choices)
     project_id = models.ForeignKey(to=Project, on_delete=models.CASCADE)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.TODO)
     author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL,on_delete=models.SET_NULL, null=True,
