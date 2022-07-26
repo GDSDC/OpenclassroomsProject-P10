d9aa68e5d0ea887af7eb7f1c1baf4ffca723751d
Antoine Latrille <antoine.latrille@gmail.com>
2022-07-26 11:32:13 +0200

    ajout d'un exemple de test

--- softdesk/api/tests.py
+++ softdesk/api/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+
+client = APIClient()
+
 
 # Create your tests here.
+def test_register():
+    request = {
+        'email': 'email@truc.fr',
+        'password1': '11111',
+        'password2': '11111',
+        'first_name': 'First',
+        'last_name': 'Last'
+    }
+    response = client.post('/signup', request, format='json')
+
+    assert response.status_code == 201
+    # TODO test the response
+    print(response.data)
+
