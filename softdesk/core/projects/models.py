9333c8d61e39d4a3e8dcf5ec4ba6344ec66a26a9
gabrieldacosta <gdc.business.2019@gmail.com>
2022-08-02 13:56:55 +0100

    fix(project) : adding is_author in Project class

--- softdesk/core/projects/models.py
+++ softdesk/core/projects/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from core.users.models import User
 
 
 class Project(models.Model):
@@ -9,3 +10,7 @@ class Project(models.Model):
     description = models.CharField(max_length=512, blank=True)
     type = models.CharField(max_length=60, blank=True)
     author_user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+
+    def is_author(self, author: User) -> bool:
+        """Function that check if the user is the author of the poject"""
+        return self.author_user == author
