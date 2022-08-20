260dd3d4d8d9e165c198da324993159acedbaf44
gabrieldacosta <gdc.business.2019@gmail.com>
2022-08-20 14:49:58 +0100

    refactor

--- softdesk/api/views/validation_functions.py
+++ softdesk/api/views/validation_functions.py
@@ -9,21 +9,15 @@ from core.issues import services as issues_service
 from core.projects import services as project_service
 from core.users.services import user_exists
 
-RESPONSES = {'project_not_found': {'message': 'PROJECT NOT FOUND. WRONG ID.',
-                                   'status': status.HTTP_404_NOT_FOUND},
-             'not_project_author': {'message': 'ACCESS FORBIDDEN. PLEASE CONTACT PROJECT AUTHOR.',
-                                    'status': status.HTTP_403_FORBIDDEN},
-             'user_not_found': {'message': 'USER NOT FOUND. WRONG ID.',
-                                'status': status.HTTP_404_NOT_FOUND},
-             'contributor_already_exists': {'message': 'USER IS ALREADY CONTRIBUTOR OF THIS PROJECT',
-                                            'status': status.HTTP_400_BAD_REQUEST},
-             'not_contributor': {'message': 'USER IS NOT CONTRIBUTOR OF THIS PROJECT',
-                                 'status': status.HTTP_403_FORBIDDEN},
-             'issue_not_found': {'message': 'ISSUE NOT FOUND. WRONG ID.',
-                                 'status': status.HTTP_404_NOT_FOUND},
-             'not_issue_author': {'message': 'USER IS NOT AUTHOR OF THIS ISSUE',
-                                  'status': status.HTTP_403_FORBIDDEN},
-             }
+MESSAGES = {
+    'project_not_found': 'PROJECT NOT FOUND. WRONG ID.',
+    'user_not_found': 'USER NOT FOUND. WRONG ID.',
+    'issue_not_found': 'ISSUE NOT FOUND. WRONG ID.',
+    'not_project_author': 'ACCESS FORBIDDEN. PLEASE CONTACT PROJECT AUTHOR.',
+    'not_project_contributor': 'ACCESS FORBIDDEN. USER IS NOT CONTRIBUTOR OF THIS PROJECT',
+    'not_issue_author': 'ACCESS FORBIDDEN. USER IS NOT AUTHOR OF THIS ISSUE',
+    'contributor_already_exists': 'USER IS ALREADY CONTRIBUTOR OF THIS PROJECT',
+}
 
 
 # ----------- GETTING USER BY ID ------------------
@@ -32,7 +26,7 @@ def get_user(user_id: int) -> Tuple[Optional[User], Optional[str], Optional[int]
     """Function to get a user if it exists"""
 
     if not user_exists(user_id=user_id):
-        return None, RESPONSES['user_not_found']['message'], RESPONSES['user_not_found']['status']
+        return None, MESSAGES['user_not_found'], status.HTTP_404_NOT_FOUND
 
     user = User.objects.get(id=user_id)
     return user, None, None
@@ -49,11 +43,16 @@ def get_project_and_ensure_access(project_id: int, author: Optional[User] = None
 
     project = project_service.get_project(project_id)
     if project is None:
-        return None, RESPONSES['project_not_found']['message'], status.HTTP_404_NOT_FOUND
+        return None, MESSAGES['project_not_found'], status.HTTP_404_NOT_FOUND
 
-    role = Contributor.Role.AUTHOR if author is not None else None
-    if not contributor_service.is_contributor(project, contributor=user, with_role=role):
-        return project, RESPONSES['not_contributor']['message'], status.HTTP_403_FORBIDDEN
+    if author is not None and not contributor_service.is_contributor(project,
+                                                                     contributor=user,
+                                                                     with_role=Contributor.Role.AUTHOR):
+        return project, MESSAGES['not_project_author'], status.HTTP_403_FORBIDDEN
+    elif contributor is not None and not contributor_service.is_contributor(project,
+                                                                            contributor=user,
+                                                                            with_role=Contributor.Role.CONTRIBUTOR):
+        return project, MESSAGES['not_project_contributor'], status.HTTP_403_FORBIDDEN
 
     return project, None, None
 
@@ -69,9 +68,9 @@ def get_issue_and_ensure_access(issue_id: int, author: User) \
 
     issue = issues_service.get_issue(issue_id)
     if issue is None:
-        return None, RESPONSES['issue_not_found']['message'], status.HTTP_404_NOT_FOUND
+        return None, MESSAGES['issue_not_found'], status.HTTP_404_NOT_FOUND
 
     if not issue.author_user == user:
-        return issue, RESPONSES['not_issue_author']['message'], status.HTTP_403_FORBIDDEN
+        return issue, MESSAGES['not_issue_author'], status.HTTP_403_FORBIDDEN
 
     return issue, None, None
