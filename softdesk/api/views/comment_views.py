91515d9e8fb0392cec0497aec24e5219274f819a
gabrieldacosta <gdc.business.2019@gmail.com>
2022-08-21 09:35:21 +0100

    feat(comments) : delete

--- softdesk/api/views/comment_views.py
+++ softdesk/api/views/comment_views.py
@@ -79,10 +79,63 @@ class Comments(APIView):
             return JsonResponse(error_message, safe=False, status=error_code)
 
         # comment error case
-        comment, error_message, error_code = get_comment_and_ensure_access(comment_id=comment_id, issue=issue,
-                                                                           author=user)
+        comment, error_message, error_code = get_comment_and_ensure_access(comment_id=comment_id, issue=issue)
         if error_message:
             return JsonResponse(error_message, safe=False, status=error_code)
 
         # get comment data
         return JsonResponse(self.serializer_class(comment).data, safe=False, status=status.HTTP_200_OK)
+
+    def put(self, request, project_id, issue_id, comment_id):
+        """Update comment"""
+
+        user = request.user
+        comment_updated_data = request.data
+
+        # project error case
+        project, error_message, error_code = get_project_and_ensure_access(project_id=project_id, contributor=user)
+        if error_message:
+            return JsonResponse(error_message, safe=False, status=error_code)
+
+        # issue error case
+        issue, error_message, error_code = get_issue_and_ensure_access(issue_id=issue_id, project=project)
+        if error_message:
+            return JsonResponse(error_message, safe=False, status=error_code)
+
+        # comment error case
+        comment_to_update, error_message, error_code = get_comment_and_ensure_access(comment_id=comment_id, issue=issue,
+                                                                                     author=user)
+        if error_message:
+            return JsonResponse(error_message, safe=False, status=error_code)
+
+        # update comment data
+        serializer = self.serializer_class(comment_to_update, data=comment_updated_data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, project_id, issue_id, comment_id):
+        """Delete comment"""
+
+        user = request.user
+
+        # project error case
+        project, error_message, error_code = get_project_and_ensure_access(project_id=project_id, contributor=user)
+        if error_message:
+            return JsonResponse(error_message, safe=False, status=error_code)
+
+        # issue error case
+        issue, error_message, error_code = get_issue_and_ensure_access(issue_id=issue_id, project=project)
+        if error_message:
+            return JsonResponse(error_message, safe=False, status=error_code)
+
+        # comment error case
+        comment_to_delete, error_message, error_code = get_comment_and_ensure_access(comment_id=comment_id, issue=issue,
+                                                                                     author=user)
+        if error_message:
+            return JsonResponse(error_message, safe=False, status=error_code)
+
+        # delete comment
+        comment_to_delete.delete()
+        return JsonResponse(f"COMMENT '{comment_id}' DELETED !", safe=False, status=status.HTTP_200_OK)
+
